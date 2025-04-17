2efa33f3bc124d09e18e3da8ca5be9a642288e0b
aimansharief <aiman.sharief.17@gmail.com>
2025-04-17 23:45:08 +0530

    fix : Removed all references to deleted setup/teardown files from the Jest config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  setupFiles: ['<rootDir>/jest.test-entities-setup.js'],
-  globalSetup: '<rootDir>/jest.global-setup.js',
-  globalTeardown: '<rootDir>/jest.global-teardown.js',
+
   roots: ['<rootDir>/src'],
   setupFilesAfterEnv: ['<rootDir>/src/__tests__/test-setup-helper.ts'],
   testMatch: ['**/__tests__/**/*.[jt]s?(x)', '!**/__tests__/test-setup-helper.ts'],
